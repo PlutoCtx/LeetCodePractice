32f7e2ace3e6319cc93585cc7400715931b5e120
PlutoCtx <chentingxian195467@163.com>
2024-04-01 00:18:59 +0800

    2024.4.1: Q

---
+++ src/main/java/The500/P100/P20/Q2/ListNode.java
@@ -0,0 +1,20 @@
+package The500.P100.P20.Q2;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/1 0:17
+ * @since JDK17
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+
+}
+
---
+++ src/main/java/The500/P100/P20/Q2/Solution.java
@@ -0,0 +1,40 @@
+package The500.P100.P20.Q2;
+
+/**
+ * LeetCodePractice
+ * 两数相加
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/1 0:17
+ * @since JDK17
+ */
+
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
+        }
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+}
+
+
