1cc44f8e9edc5a72766b25bde9d15c226fd7725a
PlutoCtx <chentingxian195467@163.com>
2024-03-31 23:49:44 +0800

    2024.3.231: Q66

--- src/main/java/The500/P100/P80/Q66/Solution.java
+++ src/main/java/The500/P100/P80/Q66/Solution.java
@@ -13,30 +13,48 @@ public class Solution {
 
 
     public int[] plusOne(int[] digits) {
-//        int len = digits.length;
-//        String Foo = null;
-//        Long sum = Foo.valueOf(0L);
-//        int k = 1;
-//
-//        for (int i = len - 1; i >= 0; i--) {
-//            sum += (long) digits[i] * k;
-//            k *= 10;
-//        }
-//        sum += 1;
-//        Long s = sum;
-//
-//        return initArray(s);
-        return null;
-    }
-
-
-    public static int[] initArray(Long num) {
-        String str = num.toString();
-        int[] array = new int[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            array[i] = Integer.parseInt(Character.toString(ch));
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
         }
-        return array;
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
+
+
+
+
+
+
+//    public int[] plusOne(int[] digits) {
+////        int len = digits.length;
+////        String Foo = null;
+////        Long sum = Foo.valueOf(0L);
+////        int k = 1;
+////
+////        for (int i = len - 1; i >= 0; i--) {
+////            sum += (long) digits[i] * k;
+////            k *= 10;
+////        }
+////        sum += 1;
+////        Long s = sum;
+////
+////        return initArray(s);
+//        return null;
+//    }
+//
+//
+//    public static int[] initArray(Long num) {
+//        String str = num.toString();
+//        int[] array = new int[str.length()];
+//        for (int i = 0; i < str.length(); i++) {
+//            char ch = str.charAt(i);
+//            array[i] = Integer.parseInt(Character.toString(ch));
+//        }
+//        return array;
+//    }
 }
