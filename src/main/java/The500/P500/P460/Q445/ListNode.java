b5652b63257d7fe9a4a744679001de41a2257771
PlutoCtx <chentingxian195467@163.com>
2024-04-01 00:21:46 +0800

    2024.4.1: Q445

---
+++ src/main/java/The500/P500/P460/Q445/ListNode.java
@@ -0,0 +1,26 @@
+package The500.P500.P460.Q445;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/1 0:20
+ * @since JDK17
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+
+    }
+
+}
