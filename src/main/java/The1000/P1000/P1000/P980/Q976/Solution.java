d89e153a2b3b93742ab0675cf6b65ad6259a3478
PlutoCtx <chentingxian195467@163.com>
2024-04-01 00:09:35 +0800

    2024.4.1: Q976

--- src/main/java/The1000/P1000/P1000/P980/Q976/Solution.java
+++ src/main/java/The1000/P1000/P1000/P980/Q976/Solution.java
@@ -1,5 +1,7 @@
 package The1000.P1000.P1000.P980.Q976;
 
+import java.util.Arrays;
+
 /**
  * LeetCodePractice
  * 三角形的最大周长
@@ -10,30 +12,24 @@ package The1000.P1000.P1000.P980.Q976;
  */
 
 public class Solution {
-    public int largestPerimeter(int[] nums) {
-        int len = nums.length;
-        int max = 0;
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                for (int k = j + 1; j < len; j++) {
-                    if (isTriangle(i, j, k)) {
-                        if (max < nums[i] + nums[j] + nums[k]) {
-                            max = nums[i] + nums[j] + nums[k];
-                        }
-                    }
+
+
+
+        public int largestPerimeter(int[] nums) {
+            Arrays.sort(nums);
+            int len = nums.length;
+            for (int i = len - 1; i > 1; i--) {
+                int a = nums[i];
+                int b = nums[i - 1];
+                int c = nums[i - 2];
+                if (b + c > a) {
+                    return a + b + c;
                 }
             }
+            return 0;
         }
-        return max;
 
-    }
 
-    public boolean isTriangle(int a, int b, int c) {
-        if (a > 0 && b > 0 && c > 0) {
-            return a + b > c && a + c > b && b + c > a;
-        } else {
-            return false;
-        }
-    }
+
 
 }
