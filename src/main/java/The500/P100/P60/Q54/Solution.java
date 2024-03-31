a463f3c3ff3dbce44f952582b3182d6a2256822d
PlutoCtx <chentingxian195467@163.com>
2024-04-01 00:05:50 +0800

    2024.3.231: Q54

---
+++ src/main/java/The500/P100/P60/Q54/Solution.java
@@ -0,0 +1,41 @@
+package The500.P100.P60.Q54;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCodePractice
+ * 螺旋矩阵
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/1 0:04
+ * @since JDK17
+ */
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> order = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        boolean[][] visited = new boolean[rows][columns];
+        int total = rows * columns;
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int directionIndex = 0;
+        for (int i = 0; i < total; i++) {
+            order.add(matrix[row][column]);
+            visited[row][column] = true;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= rows || nextColumn < 0 || nextColumn >= columns || visited[nextRow][nextColumn]) {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            row += directions[directionIndex][0];
+            column += directions[directionIndex][1];
+        }
+        return order;
+    }
+}
+
+
