c261238696f62476135cc35c6169f650c194948f
PlutoCtx <chentingxian195467@163.com>
2024-04-01 00:16:52 +0800

    2024.4.1: Q206

--- src/main/java/The500/P300/P220/Q206/Solution.java
+++ src/main/java/The500/P300/P220/Q206/Solution.java
@@ -17,21 +17,34 @@ package The500.P300.P220.Q206;
 public class Solution {
 
     public ListNode reverseList(ListNode head) {
-        ListNode temp = new ListNode();
-        temp = head.next.next;
-        ListNode cur = new ListNode();
-        ListNode pre = new ListNode();
-        ListNode pos = new ListNode();
-        ListNode res = new ListNode();
-
-        while(temp.next != null) {
-            pre = head.next;
-            temp = pre.next;
-            pre.next = head;
-            head.next = null;
+        ListNode cur = head, pre = null;
+        while(cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
         }
         return pre;
     }
+
+
+
+//    public ListNode reverseList(ListNode head) {
+//        ListNode temp = new ListNode();
+//        temp = head.next.next;
+//        ListNode cur = new ListNode();
+//        ListNode pre = new ListNode();
+//        ListNode pos = new ListNode();
+//        ListNode res = new ListNode();
+//
+//        while(temp.next != null) {
+//            pre = head.next;
+//            temp = pre.next;
+//            pre.next = head;
+//            head.next = null;
+//        }
+//        return pre;
+//    }
 }
 
 
