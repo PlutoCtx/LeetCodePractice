8d952686ee27c816dff31fe7355bc434d0da1efd
PlutoCtx <chentingxian195467@163.com>
2024-04-04 23:31:32 +0800

    2024.4.4: Q138

---
+++ src/main/java/The500/P200/P140/Q138/Node.java
@@ -0,0 +1,19 @@
+package The500.P200.P140.Q138;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/4 23:29
+ * @since JDK17
+ */
+
+public class Node {
+    public String val;
+    public Node random;
+    String str;
+    Node pre;
+    Node next;
+    Node(String str) {this.str = str;}
+    
+}
