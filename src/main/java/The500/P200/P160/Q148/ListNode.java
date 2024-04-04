2c0ff75b7b21b957a333a5247f898c9fc494f6bd
PlutoCtx <chentingxian195467@163.com>
2024-04-04 23:27:56 +0800

    2024.4.4: Q148

---
+++ src/main/java/The500/P200/P160/Q148/ListNode.java
@@ -0,0 +1,25 @@
+package The500.P200.P160.Q148;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/4 23:26
+ * @since JDK17
+ */
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
---
+++ src/main/java/The500/P200/P160/Q148/Solution.java
@@ -0,0 +1,43 @@
+package The500.P200.P160.Q148;
+
+/**
+ * LeetCodePractice
+ * 排序链表
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/4 23:26
+ * @since JDK17
+ */
+
+public class Solution {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
+}
