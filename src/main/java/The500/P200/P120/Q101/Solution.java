b391aaf314acc38c8b2285ec8e33989b54455ac0
PlutoCtx <chentingxian195467@163.com>
2024-04-05 16:41:47 +0800

    2024.4.5: Q101

---
+++ src/main/java/The500/P200/P120/Q101/Solution.java
@@ -0,0 +1,52 @@
+package The500.P200.P120.Q101;
+
+import java.util.LinkedList;
+
+/**
+ * LeetCodePractice
+ * 对称二叉树
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/5 16:39
+ * @since JDK17
+ */
+
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null || (root.left==null && root.right==null)) {
+            return true;
+        }
+        //用队列保存节点
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        //将根节点的左右孩子放到队列中
+        queue.add(root.left);
+        queue.add(root.right);
+        while(queue.size()>0) {
+            //从队列中取出两个节点，再比较这两个节点
+            TreeNode left = queue.removeFirst();
+            TreeNode right = queue.removeFirst();
+            //如果两个节点都为空就继续循环，两者有一个为空就返回false
+            if(left==null && right==null) {
+                continue;
+            }
+            if(left==null || right==null) {
+                return false;
+            }
+            if(left.val!=right.val) {
+                return false;
+            }
+            //将左节点的左孩子， 右节点的右孩子放入队列
+            queue.add(left.left);
+            queue.add(right.right);
+            //将左节点的右孩子，右节点的左孩子放入队列
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+
+        return true;
+    }
+
+
+
+}
---
+++ src/main/java/The500/P200/P120/Q101/TreeNode.java
@@ -0,0 +1,22 @@
+package The500.P200.P120.Q101;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/5 16:40
+ * @since JDK17
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
