8553f7620842dc192948bd99d7e4abb9e66bf232
PlutoCtx <chentingxian195467@163.com>
2024-04-05 16:25:23 +0800

    2024.4.5: Q25

---
+++ src/main/java/The500/P100/P40/Q25/ListNode.java
@@ -0,0 +1,23 @@
+package The500.P100.P40.Q25;
+
+import java.util.List;
+
+/**
+ * LeetCodePractice
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/5 16:22
+ * @since JDK17
+ */
+
+public class ListNode {
+
+    ListNode next;
+    int value;
+
+    public ListNode(){}
+
+    public ListNode(int val) {
+        this.value = val;
+    }
+}
---
+++ src/main/java/The500/P100/P40/Q25/Solution.java
@@ -0,0 +1,54 @@
+package The500.P100.P40.Q25;
+
+/**
+ * LeetCodePractice
+ * K 个一组翻转链表
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/5 16:21
+ * @since JDK17
+ */
+
+public class Solution {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+
+
+}
