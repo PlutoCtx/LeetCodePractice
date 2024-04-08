4ed9a563e152d892f7edc267bed61510f328384d
PlutoCtx <chentingxian195467@163.com>
2024-04-08 11:16:48 +0800

    2024.4.8: Q5

---
+++ src/main/java/The500/P100/P20/Q5/Solution.java
@@ -0,0 +1,46 @@
+package The500.P100.P20.Q5;
+
+/**
+ * LeetCodePractice
+ * 最长回文子串
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2024/4/8 11:15
+ * @since JDK17
+ */
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        int strLen = s.length();
+        int maxStart = 0;  //最长回文串的起点
+        int maxEnd = 0;    //最长回文串的终点
+        int maxLen = 1;  //最长回文串的长度
+
+        boolean[][] dp = new boolean[strLen][strLen];
+
+        for (int r = 1; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+
+                    }
+                }
+
+            }
+
+        }
+        return s.substring(maxStart, maxEnd + 1);
+
+    }
+
+
+
+}
