5a4103a245ee4aed0830f5446d664f7906628eb8
PlutoCtx <chentingxian195467@163.com>
2024-09-08 17:00:36 +0800

    2024.9.8: update Q977

--- src/main/java/The1000/P1000/P1000/P980/Q977/Solution.java
+++ src/main/java/The1000/P1000/P1000/P980/Q977/Solution.java
@@ -15,67 +15,41 @@ import java.util.List;
 public class Solution {
 
     /**
-     * 有序数组的平方
-     * <p>
-     *      执行用时分布 344 ms
-     *      击败 5.02%
-     *      消耗内存分布 45.44 MB
-     *      击败  90.84%
-     * </p>
+     * 官方的代码，在时间复杂度这一块确实没话说
      * @param nums      原始数组，有正有负
      * @return          原数组的平方，并完成升序排序
      */
     public int[] sortedSquares(int[] nums) {
-        List<Integer> squareNumber = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            squareNumber.add(nums[i] * nums[i]);
-        }
-        int[] res = squareNumber.stream().filter(integer -> integer != null).mapToInt(i -> i).toArray();
-
-        sortList(res);
-        return res;
-
-    }
-
-    /**
-     * 二院选择排序
-     * @param arr   待排序数组
-     */
-    private void sortList(int[] arr) {
-
-        int minIndex, maxIndex;
-        // i 只需要遍历一半
-        for (int i = 0; i < arr.length / 2; i++) {
-            minIndex = i;
-            maxIndex = i;
-            for (int j = i + 1; j < arr.length - i; j++) {
-                if (arr[minIndex] > arr[j]) {
-                    // 记录最小值的下标
-                    minIndex = j;
-                }
-                if (arr[maxIndex] < arr[j]) {
-                    // 记录最大值的下标
-                    maxIndex = j;
-                }
-            }
-            // 如果 minIndex 和 maxIndex 都相等，那么他们必定都等于 i，且后面的所有数字都与 arr[i] 相等，此时已经排序完成
-            if (minIndex == maxIndex) {
+        int n = nums.length;
+        int negative = -1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < 0) {
+                negative = i;
+            } else {
                 break;
             }
-            // 将最小元素交换至首位
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-            // 如果最大值的下标刚好是 i，由于 arr[i] 和 arr[minIndex] 已经交换了，所以这里要更新 maxIndex 的值。
-            if (maxIndex == i) {
-                maxIndex = minIndex;
-            }
-            // 将最大元素交换至末尾
-            int lastIndex = arr.length - 1 - i;
-            temp = arr[lastIndex];
-            arr[lastIndex] = arr[maxIndex];
-            arr[maxIndex] = temp;
         }
+
+        int[] ans = new int[n];
+        int index = 0, i = negative, j = negative + 1;
+        while (i >= 0 || j < n) {
+            if (i < 0) {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            } else if (j == n) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else if (nums[i] * nums[i] < nums[j] * nums[j]) {
+                ans[index] = nums[i] * nums[i];
+                --i;
+            } else {
+                ans[index] = nums[j] * nums[j];
+                ++j;
+            }
+            ++index;
+        }
+
+        return ans;
     }
 
 
